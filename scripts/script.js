57f3294c887e2852b54cb7110f83da062df3c19a
Daria-Bragina <160759432+Daria-Bragina@users.noreply.github.com>
2024-11-20 13:55:15 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.getElementById('btn1').onclick = function () {
+    document.getElementById('price').scrollIntoView({behavior: "smooth"});
+};
+document.getElementById('btn2').onclick = function () {
+    document.getElementById('price').scrollIntoView({behavior: "smooth"});
+};
